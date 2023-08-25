9f32fe3525598e8eb164b937ca2d4244824d5a05
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-08-25 22:45:15 +0900

    Sample3_5

---
+++ 4/Exercise4_6.java
@@ -0,0 +1,9 @@
+class Exercise4_6 {
+  public static void main(String[] args) {
+    int num = 1;
+    for (num = 0; num < 3; ++num) {
+      num *= 2;
+    }
+    System.out.println("num = " + (num++));
+  }
+}
