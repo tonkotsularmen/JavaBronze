6f56525b722815f763b8ba844f4014ee4bf07b0e
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-09-03 20:27:16 +0900

    メンバ間のアクセスのルール

---
+++ 6/Sample6_7.java
@@ -0,0 +1,25 @@
+class Sample6_7 {
+  int instanceVal;
+  static int staticVal;
+
+  int methodA() {
+    return instanceVal;
+  }
+
+  int methodB() {
+    return staticVal;
+  }
+
+  // static int methodC {
+  // return instanceVal;
+  // }
+
+  static int methodD() {
+    return staticVal;
+  }
+
+  static int methodE() {
+    Sample6_7 obj = new Sample6_7();
+    return obj.instanceVal;
+  }
+}
